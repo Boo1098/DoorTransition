04899a4a598d3ae85cdf5a540fe8a71c23904020
Nathan <nathansd1098@gmail.com>
2019-05-24 22:42:52 -0400

    Removed magic numbers

--- Playtest.ino
+++ Playtest.ino
@@ -10,15 +10,16 @@
 #define STATUS_LED 7
 #define LIGHT_POT A3
 #define SWITCH 5
+#define AMP_SD 6
 
 TMRpcm music;
 
 void setup() {
   pinMode(REED_PIN, INPUT_PULLUP);
   pinMode(SWITCH, INPUT_PULLUP);
-  pinMode(6, OUTPUT);
+  pinMode(AMP_SD, OUTPUT);
   pinMode(STATUS_LED, OUTPUT);
-  digitalWrite(6,LOW);
+  digitalWrite(AMP_SD, LOW);
   music.speakerPin = SPEAKER_PIN;
   Serial.begin(9600);
 
@@ -43,9 +44,11 @@ void setup() {
   while (true) {
     // Make sure it's not night
     if (analogRead(PR_PIN) > analogRead(LIGHT_POT) && digitalRead(SWITCH) == HIGH) {
-        digitalWrite(STATUS_LED, HIGH);
+      digitalWrite(STATUS_LED, HIGH);
+
       // Check if door is open or not
       door = digitalRead(REED_PIN);
+
       if (door == HIGH && !doorOpen) {
         // Reset random seed
         randomSeed(millis());
@@ -58,12 +61,12 @@ void setup() {
 
         // Plays song
         music.play(string2char(file));
-        digitalWrite(6,HIGH);
+        digitalWrite(AMP_SD,HIGH);
         while (music.isPlaying() == 1 && digitalRead(REED_PIN) == HIGH) {
         }
 
         // When song is done, or door is shut, ends song
-        digitalWrite(6,LOW);
+        digitalWrite(AMP_SD, LOW);
         music.disable();
         doorOpen = true;
 
